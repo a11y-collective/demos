5843040b15c097170743b831ba76d5b149502dae
fabiobindels <fabio@level-level.com>
2023-08-10 15:02:38 +0200

    Update first rule js

--- en/aria/first-rule/script.js
+++ en/aria/first-rule/script.js
@@ -1,4 +1,7 @@
-function showAlert() {
-    alert("Hello world!");
-}
+const alertButtons = document.querySelectorAll(".js-alert");
 
+alertButtons.forEach((button) => {
+	button.addEventListener("click", () => {
+		alert("You clicked me!");
+	});
+});
