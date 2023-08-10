63bd9b5497d50bafbeea33824b2f8279f1962acb
fabiobindels <fabio@level-level.com>
2023-08-10 15:02:55 +0200

    update dynamic changes js

--- en/aria/dynamic-changes/script.js
+++ en/aria/dynamic-changes/script.js
@@ -1,3 +1,14 @@
-// Write your custom JavaScript code here.
-``;
+const textarea = document.querySelector(".js-message");
+const charCountSpan = document.querySelector(".js-count");
+const maxChars = 150;
 
+textarea.addEventListener("input", updateCharCount);
+
+const updateCharCount = () => {
+	const remainingChars = maxChars - textarea.value.length;
+	charCountSpan.textContent = remainingChars;
+
+	if (remainingChars <= 0) {
+		textarea.value = textarea.value.substring(0, maxChars - 1);
+	}
+};
