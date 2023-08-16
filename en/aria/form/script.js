970d2a99c9e01df8b61309d96a0e886b603c6c12
fabiobindels <fabio@level-level.com>
2023-08-16 15:21:17 +0200

    Adds succes message to form

--- en/aria/form/script.js
+++ en/aria/form/script.js
@@ -1,60 +1,62 @@
-
 const requiredFields = document.querySelectorAll("[required]");
 const form = document.querySelector(".js-form");
-const summaryBox = document.createElement("div");
-const summaryText = document.createElement("p");
-const summaryList = document.createElement("ol");
 
 const checkValidity = (event) => {
-    event.preventDefault();
-    const emptyFields = [];
+	event.preventDefault();
+	const emptyFields = [];
 
-    requiredFields.forEach((field) => {
-        field.setAttribute('aria-invalid', 'false');
+	requiredFields.forEach((field) => {
+		field.setAttribute("aria-invalid", "false");
 
-        // Check if field is empty
-        if (!field.value) {
-            field.setAttribute('aria-invalid', 'true');
-            emptyFields.push(field.labels[0]);
-        }
-    });
+		// Check if field is empty
+		if (!field.value) {
+			field.setAttribute("aria-invalid", "true");
+			emptyFields.push(field.labels[0]);
+		}
+	});
 
-    if (!emptyFields.length) {
-        form.submit();
-    } else {
-        setSummary(emptyFields);
-        setInlineMessage(emptyFields);
-    }
+	if (!emptyFields.length) {
+		form.innerHTML = "<p>Form succesfully send!</p>"; // Success message
+	} else {
+		setSummary(emptyFields);
+		setInlineMessage(emptyFields);
+	}
 };
 
 const setSummary = (fields) => {
-    summaryBox.classList.add("form__summary");
-    summaryText.textContent =
-        "There was a problem with your submission. The following inputs need attention";
+	const summaryBox = document.createElement("div");
+	const summaryText = document.createElement("p");
+	const summaryList = document.createElement("ol");
 
-    const listItems = fields.map((field) => {
-        return `
+	summaryBox.classList.add("form__summary");
+	summaryText.textContent =
+		"There was a problem with your submission. The following inputs need attention";
+
+	const listItems = fields.map((field) => {
+		return `
 			<li>
-				<a href="#${field.getAttribute("for")}">${field.querySelector('.form__field-name').textContent}: ${field.dataset.validation}</a>
+				<a href="#${field.getAttribute("for")}">
+					${field.querySelector(".form__field-name").textContent}: ${
+			field.dataset.validation
+		}
+				</a>
 			</li>
 		`;
-    });
+	});
 
-    summaryList.innerHTML = listItems.join("");
+	summaryList.innerHTML = listItems.join("");
 
-    summaryBox.append(summaryText, summaryList);
-    summaryBox.setAttribute("tabindex", "0");
-    form.prepend(summaryBox);
-    summaryBox.focus();
+	summaryBox.append(summaryText, summaryList);
+	summaryBox.setAttribute("tabindex", "0");
+	form.prepend(summaryBox);
+	summaryBox.focus();
 };
 
 const setInlineMessage = (fields) => {
-    fields.forEach((field) => {
-        const message = field.nextElementSibling.nextElementSibling;
-        message.textContent = field.dataset.validation;
-    });
+	fields.forEach((field) => {
+		const message = field.nextElementSibling.nextElementSibling;
+		message.textContent = field.dataset.validation;
+	});
 };
 
 form.addEventListener("submit", checkValidity);
-
-
