329fec0fdda14a958db5731aa3cae9927556a4d9
fabiobindels <fabio@level-level.com>
2023-08-16 15:10:29 +0200

    Adds styling and js for aria-expanded lesson

--- en/aria/aria-expanded/script.js
+++ en/aria/aria-expanded/script.js
@@ -1,1 +1,14 @@
-// Write your custom JavaScript code here...
+const toggle = document.querySelector(".js-toggle");
+const content = document.querySelector(".js-content");
+
+toggle.addEventListener("click", function () {
+	const isExpanded = toggle.getAttribute("aria-expanded") === "true";
+
+	if (isExpanded) {
+		toggle.setAttribute("aria-expanded", "false");
+		content.setAttribute("aria-hidden", "true");
+	} else {
+		toggle.setAttribute("aria-expanded", "true");
+		content.setAttribute("aria-hidden", "false");
+	}
+});
