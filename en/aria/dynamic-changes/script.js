867c0ced94eae8209356913e4890b78875b79882
fabiobindels <fabio@level-level.com>
2023-08-21 10:58:11 +0200

    Adds aria-live updates

--- en/aria/dynamic-changes/script.js
+++ en/aria/dynamic-changes/script.js
@@ -1,6 +1,9 @@
 const textarea = document.querySelector(".js-message");
 const charCountSpan = document.querySelector(".js-count");
+const ariaLive = document.querySelector(".js-aria-live");
 const maxChars = 150;
+const halfThreshold = maxChars * 0.5;
+const ninthThreshold = maxChars * 0.1;
 
 const updateCharCount = () => {
 	let remainingChars = maxChars - textarea.value.length;
@@ -11,6 +14,20 @@ const updateCharCount = () => {
 	}
 
 	charCountSpan.textContent = remainingChars;
+
+	if (remainingChars === halfThreshold) {
+		ariaLive.setAttribute("aria-live", "polite");
+		ariaLive.innerHTML = `<p>${remainingChars} characters remaining</p>`;
+
+		return;
+	}
+
+	if (remainingChars === ninthThreshold) {
+		ariaLive.setAttribute("aria-live", "assertive");
+		ariaLive.innerHTML = `<p>${remainingChars} characters remaining</p>`;
+
+		return;
+	}
 };
 
 textarea.addEventListener("input", updateCharCount);
