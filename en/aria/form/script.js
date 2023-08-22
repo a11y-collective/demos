53191f330068005b39b87d2be41638163181ac46
rianrietveld <rian@level-level.com>
2023-08-22 14:59:35 +0200

    form

--- en/aria/form/script.js
+++ en/aria/form/script.js
@@ -1,5 +1,6 @@
 const requiredFields = document.querySelectorAll("[required]");
-const form = document.querySelector(".js-form");
+const form           = document.querySelector(".js-form");
+const notifications  = document.querySelector(".js-notifications");
 
 const checkValidity = (event) => {
 	event.preventDefault();
@@ -8,29 +9,32 @@ const checkValidity = (event) => {
 	requiredFields.forEach((field) => {
 		field.setAttribute("aria-invalid", "false");
 
-		// Check if field is empty
+		// Check if field is empty.
 		if (!field.value) {
 			field.setAttribute("aria-invalid", "true");
 			emptyFields.push(field.labels[0]);
 		}
 	});
 
+	notifications.innerHTML = "";
+
 	if (!emptyFields.length) {
-		form.innerHTML = "<p>Form succesfully send!</p>"; // Success message
+		notifications.innerHTML = "<p>Form successfully send!</p>"; // Success message.
 	} else {
 		setSummary(emptyFields);
 		setInlineMessage(emptyFields);
 	}
+	notifications.focus();
 };
 
 const setSummary = (fields) => {
-	const summaryBox = document.createElement("div");
-	const summaryText = document.createElement("p");
-	const summaryList = document.createElement("ol");
+
+	const summaryBox     = document.createElement("div");
+	const summaryMessage = document.createElement("h3");
+	const summaryText    = document.createElement("p");
+	const summaryList    = document.createElement("ol");
 
 	summaryBox.classList.add("form__summary");
-	summaryText.textContent =
-		"There was a problem with your submission. The following inputs need attention";
 
 	const listItems = fields.map((field) => {
 		return `
@@ -43,13 +47,10 @@ const setSummary = (fields) => {
 			</li>
 		`;
 	});
-
-	summaryList.innerHTML = listItems.join("");
-
-	summaryBox.append(summaryText, summaryList);
-	summaryBox.setAttribute("tabindex", "0");
-	form.prepend(summaryBox);
-	summaryBox.focus();
+	summaryMessage.innerHTML = "There was a problem with your submission. The following inputs need attention"; // Fail message.
+	summaryList.innerHTML    = listItems.join("");
+	summaryBox.append(summaryMessage, summaryText, summaryList);
+	notifications.prepend(summaryBox);
 };
 
 const setInlineMessage = (fields) => {
